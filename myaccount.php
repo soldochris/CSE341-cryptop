1375903bd48a925b8417d874602ecdd808494e71
soldochris <soldevilla.christian@gmail.com>
2021-02-13 01:59:26 -0500

    logging

--- myaccount.php
+++ myaccount.php
@@ -22,5 +22,26 @@
   array_pop($userData);
 
   $_SESSION['userData'] = $userData;
-  var_dump($_SESSION['userData']);
+
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Crypto - My Account</title>
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <?php require_once('modules/navbar.php'); ?>
+
+  <main class="container container-fluid mt-4">
+    <?php var_dump($_SESSION['userData'); ?>
+  </main>
+
+  <?php require_once('modules/footer.php'); ?>
+
+  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</body>
+</html>
